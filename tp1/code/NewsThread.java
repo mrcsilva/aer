ffbb51a6dc9b7a053829c0dcb5d3eb558f1a7d18
Mário Silva <mario156silva@gmail.com>
2018-03-26 12:49:16 +0100

    Finished NEWS_FOR

--- tp1/code/NewsThread.java
+++ tp1/code/NewsThread.java
@@ -26,6 +26,13 @@ class NewsThread extends Thread {
     public void run() {
         MulticastSocket so = null;
         InetAddress group = null;
+        String dest = "";
+        InetAddress dip = null;
+        byte[] buf = new byte[500];
+        DatagramPacket p = null;
+        String source = "";
+        String self = "";
+        String dataTcp = "";
         try {
             so = new MulticastSocket(9999);
             group = InetAddress.getByName("FF02::1");
@@ -36,19 +43,103 @@ class NewsThread extends Thread {
 
         // flag=1 => NEWS_FOR | flag=0 => GET_NEWS_FROM
         if(flag == 1) {
-
-        }
-        else {
-            String self = "";
             for(No n : tabela.values()) {
                 if(n.getSaltos() == 0) {
                     self = n.getIp().getHostAddress();
                     break;
                 }
             }
+            // Coloca numa string o conteudo a enviar
+            for (int i = 3; i < data.length; i++) {
+                dataTcp += data[i]+" ";
+            }
+            // Se o vizinho for o desejado
             if(self.equals(data[3])) {
-                String dest = data[2];
-                InetAddress dip = null;
+                try {
+                    dest = data[2];
+                    try {
+                        dip = InetAddress.getByName(dest);
+                    }
+                    catch(Exception e) {
+                        System.out.println("Get DIP: " + e.getMessage());
+                    }
+                    // Se o destino estiver na tabela e não for o próprio
+                    // Se estiver e for o proprio
+                    // Se não estiver
+                    if(tabela.containsKey(dip) && tabela.get(dip).getSaltos() != 0){
+                        try{
+                            String temp = "NEWS_FOR " + data[1] + " " + data[2] + " " + tabela.get(dip).getIpVizinho().getHostAddress() + " " + dataTcp;
+                            buf = temp.getBytes();
+                            p = new DatagramPacket(buf, buf.length, group, 9999);
+                            so.send(p);
+                            so.leaveGroup(group);
+                            so.close();
+                            System.out.println("Sent NEWS:FOR to: " + tabela.get(dip).getIpVizinho().getHostAddress());
+                        }
+                        catch(Exception e) {
+                            System.out.println("NT Send e N!=0: " + e.getMessage());
+                        }
+                    }
+                    else if (tabela.containsKey(dip) && tabela.get(dip).getSaltos() == 0) {
+                        try {
+                            InetAddress sip = InetAddress.getByName(data[1]);
+                            Socket s = new Socket("localhost", 9999);
+                            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+                            out.println("NEWS:\n" + dataTcp);
+                            System.out.println("Delivered NEWS to: " + data[2]);
+                        }
+                        catch(Exception e) {
+                            System.out.println("NT Send e 0: " + e.getMessage());
+                        }
+                    }
+                    else if(!tabela.containsKey(dip)) {
+                        try {
+                            No no = new No(dip, dip, -1, null, 0);
+                            tabela.put(dip, no);
+                            so.joinGroup(group);
+                            for(No n : tabela.values()) {
+                                if(n.getSaltos() == 0) {
+                                    source = n.getIp().getHostAddress();
+                                    break;
+                                }
+                            }
+                            String temp = "ROUTE_REQUEST " + source + " " + dip.getHostAddress() + " " + 10 + " " + 500;
+                            buf = temp.getBytes();
+                            DatagramPacket p = new DatagramPacket(buf, buf.length, group, 9999);
+                            so.send(p);
+                            so.leaveGroup(group);
+                            so.close();
+                            Thread.sleep(550);
+                            // Parte de cima para adicionar o caminho até onde é preciso
+                            // Agora tratar de reencaminhar
+                            temp = "NEWS_FOR " + data[1] + " " + data[2] + " " + tabela.get(dip).getIpVizinho().getHostAddress() + " " + dataTcp;
+                            buf = temp.getBytes();
+                            p = new DatagramPacket(buf, buf.length, group, 9999);
+                            so.send(p);
+                            so.leaveGroup(group);
+                            so.close();
+                            System.out.println("Sent NEWS_FOR to: " + tabela.get(dip).getIpVizinho().getHostAddress());
+                        }
+                        catch(Exception e) {
+                            System.out.println("NT Send N-contem: " + e.getMessage());
+                        }
+                    }
+                }
+                catch(Exception e) {
+
+                }
+            }
+        }
+        else {
+            for(No n : tabela.values()) {
+                if(n.getSaltos() == 0) {
+                    self = n.getIp().getHostAddress();
+                    break;
+                }
+            }
+            // Se ele for o vizinho pretendido
+            if(self.equals(data[3])) {
+                dest = data[2];
                 try {
                     dip = InetAddress.getByName(dest);
                 }
@@ -58,9 +149,8 @@ class NewsThread extends Thread {
                 if(tabela.containsKey(dip) && tabela.get(dip).getSaltos() != 0){
                     try{
                         String temp = "GET_NEWS_FROM " + data[1] + " " + data[2] + " " + tabela.get(dip).getIpVizinho().getHostAddress();
-                        byte[] buf = new byte[500];
                         buf = temp.getBytes();
-                        DatagramPacket p = new DatagramPacket(buf, buf.length, group, 9999);
+                        p = new DatagramPacket(buf, buf.length, group, 9999);
                         so.send(p);
                         so.leaveGroup(group);
                         so.close();
@@ -85,7 +175,6 @@ class NewsThread extends Thread {
                         No no = new No(dip, dip, -1, null, 0);
                         tabela.put(dip, no);
                         so.joinGroup(group);
-                        String source = "";
                         for(No n : tabela.values()) {
                             if(n.getSaltos() == 0) {
                                 source = n.getIp().getHostAddress();
@@ -93,7 +182,6 @@ class NewsThread extends Thread {
                             }
                         }
                         String temp = "ROUTE_REQUEST " + source + " " + dip.getHostAddress() + " " + 10 + " " + 500;
-                        byte[] buf = new byte[500];
                         buf = temp.getBytes();
                         DatagramPacket p = new DatagramPacket(buf, buf.length, group, 9999);
                         so.send(p);
--- tp1/code/TCPThread.java
+++ tp1/code/TCPThread.java
@@ -8,6 +8,9 @@ import java.net.DatagramPacket;
 import java.lang.Exception;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.lang.InterruptedException;
 
 class TCPThread extends Thread {
 
@@ -60,103 +63,98 @@ class TCPThread extends Thread {
         String re = "";
         InetAddress group = null;
         InetAddress dip = null;
+        String source = "";
+        String viz = "";
+        String dest = "";
+        String data = "";
+        byte[] b = new byte[1000];
+        DatagramPacket packet = null;
         try {
             group = InetAddress.getByName("FF02::1");
             ss = new ServerSocket(9999);
             ms = new MulticastSocket(9999);
-        } catch(Exception e) {
+        }
+        catch(Exception e) {
             System.out.println("Group TCP: " + e.getMessage());
         }
-        try {
-            while((socket = ss.accept()) != null) {
+        while(true) {
+            // Aceitas ligações TCP para receber os dados
+            try {
+                socket = ss.accept();
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 re = in.readLine();
                 socket.shutdownOutput();
                 socket.shutdownInput();
                 socket.close();
-                if(re.split(" ")[0].equals("GET_NEWS_FROM")) {
-                    String dest = re.split(" ")[1];
+            }
+            catch(IOException e) {
+                System.out.println("Socket Accept: " + e.getMessage());
+            }
+            // Se o que recebeu foi um GET_NEWS_FROM ou um NEWS_FOR
+            if(re.split(" ")[0].equals("GET_NEWS_FROM")) {
+                dest = re.split(" ")[1];
+                try {
                     ms.joinGroup(group);
-                    if(tabela.containsKey(InetAddress.getByName(dest))) {
-                        try {
-                            String source = "";
-                            String viz = "";
-                            for(No n : tabela.values()) {
-                                if(n.getSaltos() == 0) {
-                                    source = n.getIp().getHostAddress();
-                                }
-                                else if (n.getIp().getHostAddress().equals(dest)) {
-                                    viz = n.getIpVizinho().getHostAddress();
-                                }
+                    dip = InetAddress.getByName(dest);
+                }
+                catch(IOException e) {
+                    System.out.println("GET DIP IO: " + e.getMessage());
+                }
+                // Se a tabela já contem o destino ou não
+                if(tabela.containsKey(dip)) {
+                    try {
+                        source = "";
+                        viz = "";
+                        // Obtem o seu próprio endereço e o do vizinho
+                        for(No n : tabela.values()) {
+                            if(n.getSaltos() == 0) {
+                                source = n.getIp().getHostAddress();
                             }
-                            String data = "GET_NEWS_FROM " + source + " " + dest + " " + viz;
-                            byte[] b = new byte[1000];
-                            b = data.getBytes();
-                            DatagramPacket packet = new DatagramPacket(b, b.length, group, 9999);
-                            ms.send(packet);
-                            ms.leaveGroup(group);
-                            ms.close();
+                            else if (n.getIp().getHostAddress().equals(dest)) {
+                                viz = n.getIpVizinho().getHostAddress();
+                            }
                         }
-                        catch(Exception e) {
-                            System.out.println("TCP Contem: " + e.getMessage());
-                        }
+                        data = "GET_NEWS_FROM " + source + " " + dest + " " + viz;
+                        b = data.getBytes();
+                        packet = new DatagramPacket(b, b.length, group, 9999);
+                        ms.send(packet);
+                        ms.leaveGroup(group);
+                        ms.close();
+                        System.out.println("SENT GET_NEWS_FROM to: " + viz);
                     }
-                    else {
-                        int tempo = 200;
-                        int saltos = 6;
-                        do {
-                            tempo += 150;
-                            saltos += 1;
-                            sendRR(InetAddress.getByName(dest), tempo, saltos);
-                            Thread.sleep(tempo+50);
-                        } while (!tabela.containsKey(InetAddress.getByName(dest)) && saltos <= 38 && tempo <= 5000);
-                        try {
-                            if(saltos <= 38 && tempo <= 5000) {
-                                String source = "";
-                                String viz = "";
-                                for(No n : tabela.values()) {
-                                    if(n.getSaltos() == 0) {
-                                        source = n.getIp().getHostAddress();
-                                    }
-                                    else if (n.getIp().getHostAddress().equals(dest)) {
-                                        viz = n.getIpVizinho().getHostAddress();
-                                    }
-                                }
-                                String data = "GET_NEWS_FROM " + source + " " + dest + " " + viz;
-                                byte[] b = new byte[1000];
-                                b = data.getBytes();
-                                DatagramPacket packet = new DatagramPacket(b, b.length, group, 9999);
-                                ms.send(packet);
-                                ms.leaveGroup(group);
-                                ms.close();
-                            }
-                            else {
-                                Socket s = new Socket("localhost", 9999);
-                                PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-                                out.println("Não foi encontrado nenhum servidor com esse endereço");
-                                s.shutdownOutput();
-                                s.shutdownInput();
-                                s.close();
-                            }
-                        }
-                        catch(Exception e) {
-                            System.out.println("TCP N-contem: " + e.getMessage());
-                        }
+                    catch(Exception e) {
+                        System.out.println("TCP GET Contem: " + e.getMessage());
                     }
                 }
-                else if(re.split(" ")[0].equals("NEWS_FOR")) {
-                    String dest = re.split(" ")[1];
-                    ms.joinGroup(group);
-                    dip = null;
+                else {
+                    int tempo = 200;
+                    int saltos = 6;
+                    InetAddress vizip = null;
                     try {
-                        dip = InetAddress.getByName(dest);
-                    } catch(Exception e) {
-                        System.out.println("DIP TCP NEWS: " + e.getMessage());
+                        vizip = InetAddress.getByName(dest);
                     }
-                    if(tabela.containsKey(dip)) {
+                    catch(Exception ee) {
+                        System.out.println("GET Vizinho: " + ee.getMessage());
+                    }
+                    // Enquanto não for colocada na tabela a entrada com o caminho para o destino do pedido
+                    // Enviamos ROUTE_REQUEST com um aumento de tempo e de saltos
+                    do {
+                        tempo += 150;
+                        saltos += 1;
+                        sendRR(vizip, tempo, saltos);
                         try {
-                            String source = "";
-                            String viz = "";
+                            Thread.sleep(tempo+50);
+                        }
+                        catch(InterruptedException e) {
+                            System.out.println("Erro ao Dormir GET N-contem: " + e.getMessage());
+                        }
+                    } while (!tabela.containsKey(vizip) && saltos <= 38 && tempo <= 5000);
+                    try {
+                        // Se o encontrou ou não
+                        if(saltos <= 38 && tempo <= 5000) {
+                            source = "";
+                            viz = "";
+                            // Obtem o ip do vizinho e dele próprio
                             for(No n : tabela.values()) {
                                 if(n.getSaltos() == 0) {
                                     source = n.getIp().getHostAddress();
@@ -165,64 +163,115 @@ class TCPThread extends Thread {
                                     viz = n.getIpVizinho().getHostAddress();
                                 }
                             }
-                            String data = "NEWS_FOR " + source + " " + dest + " " + viz;
-                            byte[] b = new byte[1000];
+                            data = "GET_NEWS_FROM " + source + " " + dest + " " + viz;
                             b = data.getBytes();
-                            DatagramPacket packet = new DatagramPacket(b, b.length, group, 9999);
+                            packet = new DatagramPacket(b, b.length, group, 9999);
                             ms.send(packet);
                             ms.leaveGroup(group);
                             ms.close();
+                            System.out.println("Sent GET n-contem to: " + viz);
                         }
-                        catch(Exception e) {
-                            System.out.println("TCP Send News Contem: " + e.getMessage());
+                        else {
+                            Socket s = new Socket("localhost", 9999);
+                            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+                            out.println("Não foi encontrado nenhum servidor com esse endereço");
+                            s.shutdownOutput();
+                            s.shutdownInput();
+                            s.close();
                         }
                     }
-                    else {
-                        int tempo = 200;
-                        int saltos = 6;
-                        dip = null;
-                        try {
-                            dip = InetAddress.getByName(dest);
-                        }
-                        catch(Exception e) {
-                            System.out.println("TCP DIP: " + e.getMessage());
-                        }
-                        do {
-                            tempo += 150;
-                            saltos += 1;
-                            sendRR(dip, tempo, saltos);
-                            Thread.sleep(tempo+50);
-                        } while (!tabela.containsKey(dip) && saltos <= 38 && tempo <= 5000);
-                        try {
-                            if(saltos <= 38 && tempo <= 5000) {
-                                String source = "";
-                                String viz = "";
-                                for(No n : tabela.values()) {
-                                    if(n.getSaltos() == 0) {
-                                        source = n.getIp().getHostAddress();
-                                    }
-                                    else if (n.getIp().getHostAddress().equals(dest)) {
-                                        viz = n.getIpVizinho().getHostAddress();
-                                    }
-                                }
-                                String data = "NEWS_FOR " + source + " " + dest + " " + viz;
-                                byte[] b = new byte[1000];
-                                b = data.getBytes();
-                                DatagramPacket packet = new DatagramPacket(b, b.length, group, 9999);
-                                ms.send(packet);
-                                ms.leaveGroup(group);
-                                ms.close();
-                            }
-                        }
-                        catch(Exception e) {
-                            System.out.println("TCP Send News N-contem: " + e.getMessage());
-                        }
+                    catch(Exception e) {
+                        System.out.println("TCP N-contem: " + e.getMessage());
                     }
                 }
             }
-        }
-        catch(Exception e) {
-            System.out.println("Accept: " + e.getMessage());
+            else if(re.split(" ")[0].equals("NEWS_FOR")) {
+                dest = re.split(" ")[1];
+                try {
+                    ms.joinGroup(group);
+                    dip = InetAddress.getByName(dest);
+                }
+                catch(Exception e) {
+                    System.out.println("DIP TCP NEWS: " + e.getMessage());
+                }
+                // Existe o destino na tabela ou não
+                if(tabela.containsKey(dip)) {
+                    try {
+                        source = "";
+                        viz = "";
+                        // Obtem o seu IP e o do vizinho
+                        for(No n : tabela.values()) {
+                            if(n.getSaltos() == 0) {
+                                source = n.getIp().getHostAddress();
+                            }
+                            else if (n.getIp().getHostAddress().equals(dest)) {
+                                viz = n.getIpVizinho().getHostAddress();
+                            }
+                        }
+                        data = "NEWS_FOR " + source + " " + dest + " " + viz;
+                        b = data.getBytes();
+                        packet = new DatagramPacket(b, b.length, group, 9999);
+                        ms.send(packet);
+                        ms.leaveGroup(group);
+                        ms.close();
+                    }
+                    catch(Exception e) {
+                        System.out.println("TCP Send News Contem: " + e.getMessage());
+                    }
+                }
+                else {
+                    int tempo = 200;
+                    int saltos = 6;
+                    dip = null;
+                    try {
+                        dip = InetAddress.getByName(dest);
+                    }
+                    catch(Exception e) {
+                        System.out.println("TCP DIP: " + e.getMessage());
+                    }
+                    // Descoberta do destino
+                    do {
+                        tempo += 150;
+                        saltos += 1;
+                        sendRR(dip, tempo, saltos);
+                        try {
+                            Thread.sleep(tempo+50);
+                        }
+                        catch(InterruptedException e) {
+                            System.out.println("Erro ao Dormir SEND N-contem: " + e.getMessage());
+                        }
+                    } while (!tabela.containsKey(dip) && saltos <= 38 && tempo <= 5000);
+                    try {
+                        // Se encontrou o destino
+                        if(saltos <= 38 && tempo <= 5000) {
+                            source = "";
+                            viz = "";
+                            String[] temp = re.split(" ");
+                            String dataTcp = "";
+                            for(No n : tabela.values()) {
+                                if(n.getSaltos() == 0) {
+                                    source = n.getIp().getHostAddress();
+                                }
+                                else if (n.getIp().getHostAddress().equals(dest)) {
+                                    viz = n.getIpVizinho().getHostAddress();
+                                }
+                            }
+                            for (int i = 3; i < temp.length; i++) {
+                                dataTcp += temp[i]+" ";
+                            }
+                            data = "NEWS_FOR " + source + " " + dest + " " + viz + " " + dataTcp;
+                            b = data.getBytes();
+                            packet = new DatagramPacket(b, b.length, group, 9999);
+                            ms.send(packet);
+                            ms.leaveGroup(group);
+                            ms.close();
+                        }
+                    }
+                    catch(Exception e) {
+                        System.out.println("TCP Send News N-contem: " + e.getMessage());
+                    }
+                }
+            }
         }
     }
 }
